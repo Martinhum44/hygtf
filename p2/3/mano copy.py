0d87e1bc326cc535d29d0df752764349abca73fc
Martinhum44 <martingomezmartinezostos@gmail.com>
2024-11-25 19:11:49 -0600

    terminado

--- p2/3/mano copy.py
+++ p2/3/mano copy.py
@@ -15,7 +15,7 @@ screen_width, screen_height = pyautogui.size()
 
 MPH = mp.solutions.hands
 MPD = mp.solutions.drawing_utils
-hands = MPH.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5)
+hands = MPH.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.2)
 
 tipIds = [4, 8, 12, 16, 20]
 
@@ -27,10 +27,20 @@ def calcularDistanciaEntreDedos(image, hand_lamdmarks, handNo = 0):
     if hand_lamdmarks is not None:
         landmarks = hand_lamdmarks[handNo].landmark
 
-        finger_tip_x = int((landmarks[8].x))
-        finger_tip_y = int((landmarks[8].y))
+        finger_tip_x = int((landmarks[8].x)*cap_width)
+        finger_tip_y = int((landmarks[8].y)*cap_height)
 
-        cv2.line(image, (finger_tip_x, finger_tip_y), (000, 000, 255), 2)
+        thumb_tip_x = int((landmarks[4].x)*cap_width)
+        thumb_tip_y = int((landmarks[4].y)*cap_height)
+
+        cv2.line(image, (finger_tip_x, finger_tip_y), (thumb_tip_x, thumb_tip_y),(000, 000, 255), 2)
+        try:
+            relative_x = (screen_width/cap_width)*finger_tip_x
+            relative_y = (screen_width/cap_width)*finger_tip_y
+            MOUSE.position = (relative_x,relative_y)
+            return math.sqrt((thumb_tip_y-finger_tip_y)**2 + (thumb_tip_x-finger_tip_x))
+        except Exception:
+            return 9999
 
 def drawHandLandmarks(img, hand_landmarks):
     for landmark in hand_landmarks:
@@ -47,6 +57,20 @@ while True:
     print(image, H_landmarks)
     if H_landmarks is not None:
         drawHandLandmarks(image, H_landmarks)
+        distancia = calcularDistanciaEntreDedos(image, H_landmarks)
+        cv2.putText(image, f"Distancia: {distancia}", (40,40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (000,000,000), 1)
+
+        if distancia > 40:
+            if pinch == True:
+                print("Pinch = True")
+                pinch = False
+                MOUSE.release(Button.left)
+
+        if distancia <= 40:
+            if pinch == False:
+                print("Pinch = False")
+                pinch = True
+                MOUSE.press(Button.left)
 
     if not success:
         print("OH NO D:")
